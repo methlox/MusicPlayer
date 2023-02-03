3d1e8421e72966ec31d762e48d567c294258272c
Siddhant <84025779+methlox@users.noreply.github.com>
2023-02-03 20:22:12 +0530

    completed the project

---
+++ api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause',
+                  'votes_to_skip', 'created_at')
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
+
+
+class UpdateRoomSerializer(serializers.ModelSerializer):
+    code = serializers.CharField(validators=[])
+
+    # we need to pass in unique 'code' however we aren't trying to create a new room so we redefine code above
+
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip', 'code')
---
+++ spotify/credentials.py
@@ -0,0 +1,3 @@
+CLIENT_ID = "e2f4bde6c03e4474ad279d2832e9af67"
+CLIENT_SECRET = "7ab761cf3c7f412f97d11ca2ca0c94fd"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
